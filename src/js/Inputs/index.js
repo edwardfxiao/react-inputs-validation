240c1ca2170d87832a98014cec2b2b8f2d591ec8
edward <edwardfhsiao@gmail.com>
2017-08-31 11:24:17 +0800

    Fix window error when server-side rendering is needed

--- src/js/Inputs/index.js
+++ src/js/Inputs/index.js
@@ -3,9 +3,11 @@ import Textarea from './Textarea.js';
 import Radiobox from './Radiobox.js';
 import Checkbox from './Checkbox.js';
 import Select from './Select.js';
-window.Textbox = Textbox;
-window.Textarea = Textarea;
-window.Radiobox = Radiobox;
-window.Checkbox = Checkbox;
-window.Select = Select;
+if (typeof window !== 'undefined') {
+  window.Textbox = Textbox;
+  window.Textarea = Textarea;
+  window.Radiobox = Radiobox;
+  window.Checkbox = Checkbox;
+  window.Select = Select;
+}
 export { Textbox, Textarea, Radiobox, Checkbox, Select };

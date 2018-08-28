b2f50395e6e9ebf10fb0daf6dcbde2b8ce76bf51
Edward Xiao <edwardfhsiao@gmail.com>
2018-08-28 13:43:59 +0800

    no message

--- src/js/Inputs/message.js
+++ src/js/Inputs/message.js
@@ -24,6 +24,28 @@ const TEXT_BOX_VALIDATION_EN_US = {
   twoInputsNotEqual: () => `two inputs are not equal`
 };
 
+const TEXT_AREA_VALIDATION_ZH_CN = {
+  empty: name => `${name}不能为空`,
+  invalid: name => `${name}格式有误`,
+  invalidFormat: name => `${name} 不是数字`,
+  inBetween: name => min => max => `${name}必须在${min}-${max}之间`,
+  lessThan: name => min => `${name}不可少于${min}`,
+  greaterThan: name => max => `${name}不可大于${max}`,
+  lengthEqual: name => length => `${name}长度必须为${length}`,
+  twoInputsNotEqual: () => `两次输入不一致`
+};
+
+const TEXT_AREA_VALIDATION_EN_US = {
+  empty: name => `${getEnglishName(name)}cannot be empty`,
+  invalid: name => `${getEnglishName(name)}invalid format`,
+  invalidFormat: name => `${getEnglishName(name)}is not a number`,
+  inBetween: name => min => max => `${getEnglishName(name)}must be ${min}-${max}`,
+  lessThan: name => min => `${getEnglishName(name)}cannot less than ${min}`,
+  greaterThan: name => max => `${getEnglishName(name)}cannot greater than ${max}`,
+  lengthEqual: name => length => `${getEnglishName(name)}length must be ${length}`,
+  twoInputsNotEqual: () => `two inputs are not equal`
+};
+
 const SELECT_VALIDATION_ZH_CN = {
   empty: name => `请选择一个${name}`
 };
@@ -54,14 +76,14 @@ const message = {
     radiobox: RADIO_BOX_VALIDATION_ZH_CN,
     checkbox: CHECK_BOX_VALIDATION_ZH_CN,
     select: SELECT_VALIDATION_ZH_CN,
-    textarea: TEXT_BOX_VALIDATION_ZH_CN
+    textarea: TEXT_AREA_VALIDATION_ZH_CN
   },
   'en-US': {
     textbox: TEXT_BOX_VALIDATION_EN_US,
     radiobox: RADIO_BOX_VALIDATION_EN_US,
     checkbox: CHECK_BOX_VALIDATION_EN_US,
     select: SELECT_VALIDATION_EN_US,
-    textarea: TEXT_BOX_VALIDATION_EN_US
+    textarea: TEXT_AREA_VALIDATION_EN_US
   }
 };
 
@@ -76,11 +98,13 @@ if (window.REACT_INPUTS_VALIDATION && window.REACT_INPUTS_VALIDATION['customErro
       if (!message[i]) {
         message[i] = window.REACT_INPUTS_VALIDATION['customErrorMessage'][i];
       } else {
-        if (Object.keys(window.REACT_INPUTS_VALIDATION['customErrorMessage'][i]).length > 0) {
+        if (Object.keys(window.REACT_INPUTS_VALIDATION['customErrorMessage'][i]).length) {
           Object.keys(window.REACT_INPUTS_VALIDATION['customErrorMessage'][i]).map(j => {
-            Object.keys(window.REACT_INPUTS_VALIDATION['customErrorMessage'][i][j]).map(k => {
-              message[i][j][k] = window.REACT_INPUTS_VALIDATION['customErrorMessage'][i][j][k];
-            });
+            if (Object.keys(window.REACT_INPUTS_VALIDATION['customErrorMessage'][i][j]).length) {
+              Object.keys(window.REACT_INPUTS_VALIDATION['customErrorMessage'][i][j]).map(k => {
+                message[i][j][k] = window.REACT_INPUTS_VALIDATION['customErrorMessage'][i][j][k];
+              });
+            }
           });
         }
       }

229d44212dcc9289257114049fc12b3e5876cb85
Edward Xiao <edwardfhsiao@gmail.com>
2018-09-04 13:57:34 +0800

    no message

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var ReactInputsValidation = require('./lib/components/index.js');
-require('./lib/react-inputs-validation.min.css');
 exports.Textbox = ReactInputsValidation.Textbox;
 exports.Textarea = ReactInputsValidation.Textarea;
 exports.Radiobox = ReactInputsValidation.Radiobox;

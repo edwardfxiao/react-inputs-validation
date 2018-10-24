4fca51b6199eb71c9ad1918834f0c206b276d990
Edward Xiao <edwardfhsiao@gmail.com>
2018-10-24 15:39:28 +0800

    no message

--- example/index.js
+++ example/index.js
@@ -68,7 +68,7 @@ import { Textbox } from 'react-inputs-validation';
   tabIndex="1" // Optional.[String or Number].Default: -1.
   id={'Number'} // Optional.[String].Default: "".  Input ID.
   name="Number" // Optional.[String].Default: "". Input name.
-  type="text" // Optional.[String].Default: "text". Input type [text, password, number].
+  type="text" // Optional.[String].Default: "text". Input type [text, password, number]. NOTE: provide "text" for better performance since different browsers run differently with "number".
   value={number} // Optional.[String].Default: "".
   placeholder="Place your number here ^-^" // Optional.[String].Default: "".
   onChange={(number, e) => {

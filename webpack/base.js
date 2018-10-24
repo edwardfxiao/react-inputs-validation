3e3892021b4aa2087087d6b575e33ccdb5a5cc0e
Edward Xiao <edwardfhsiao@gmail.com>
2018-10-24 14:36:47 +0800

    modify webpack config

--- webpack/base.js
+++ webpack/base.js
@@ -3,7 +3,6 @@ const path = require('path');
 const PATH = require('./build_path');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 var config = (module.exports = {
   context: PATH.ROOT_PATH,
   entry: {
@@ -182,7 +181,6 @@ var config = (module.exports = {
     compress: true,
     disableHostCheck: true,
     historyApiFallback: true,
-
     host: '0.0.0.0',
     port: 9000,
   },
@@ -199,25 +197,6 @@ var config = (module.exports = {
     }),
     new WebpackAssetsManifest({
       fileName: 'manifest-rev.json',
-    }),
-    new HtmlWebpackPlugin({
-      template: PATH.HTML_PATH + '/layout.html',
-      title: 'react-inputs-validation',
-      page: 'index',
-      filename: 'index.html',
-      hash: false,
-      chunksSortMode: function(chunk1, chunk2) {
-        var orders = ['index'];
-        var order1 = orders.indexOf(chunk1.names[0]);
-        var order2 = orders.indexOf(chunk2.names[0]);
-        if (order1 > order2) {
-          return 1;
-        } else if (order1 < order2) {
-          return -1;
-        } else {
-          return 0;
-        }
-      },
     }),
   ],
 });

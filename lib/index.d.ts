17f8725db9ecc2e3511f4aa2ca4ce0b1494dade3
edwardxiao <edwardfhsiao@gmail.com>
2022-02-13 09:59:17 +0800

    - remove lib for github

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Textbox from './Textbox';
-import Textarea from './Textarea';
-import Select from './Select';
-import Checkbox from './Checkbox';
-import Radiobox from './Radiobox';
-export { Textbox, Textarea, Select, Checkbox, Radiobox };
--- lib/validator.d.ts
+++
@@ -1,5 +0,0 @@
-interface Validator {
-    [key: string]: Function;
-}
-declare const validator: Validator;
-export default validator;
